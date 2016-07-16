429d029d5758b13627b7d4efd52a46d2041ab320
KaijuKevin <kevc.dev@gmail.com>
2016-07-15 18:35:15 -0700

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pdxflightsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pdxflightsApp
+ */
+angular.module('pdxflightsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pdxflightsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pdxflightsApp
+ */
+angular.module('pdxflightsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
